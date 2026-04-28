3885c8280781282188e3a8f5537cd6d67bc3cc6b
TeamSwordphin <jackie_yeung@protonmail.com>
2026-04-28 08:24:10 -0400

    AI agent init

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Chatbox")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=messages
+)
+
+if response.usage_metadata is not None:
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    raise RuntimeError("Response is missing usage metadata")
+
+print(response.text)
